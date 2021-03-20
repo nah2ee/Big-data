0b8a8f7bd263018e0e88dbb668b7ad926e748c68
nah2ee <dlth0815@gmail.com>
2021-03-21 04:30:57 +0900

    Banapresso 크롤링 과제 / mySql에 올라가지 않는 상태

---
+++ 210320. Jsp/banapresso.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE banapresso;
+
+USE banapresso;
+
+CREATE TABLE tb_store (
+	sto_idx bigint auto_increment primary key,
+    sto_name varchar(20) not null,
+    sto_address varchar(50) not null
+);
+
+SELECT * FROM tb_store;
+DROP TABLE tb_store;
+
+INSERT INTO tb_store(sto_name, sto_address) VALUES ("사과점", "서울시 강남구");
